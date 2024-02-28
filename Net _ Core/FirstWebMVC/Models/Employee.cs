14bb6bf7829ef06c362a6db33e68dd677e18cbab
“PHAM <2021050646@student.humg.edu.vn>
2024-02-28 23:08:20 +0700

    MVC

---
+++ Net _ Core/FirstWebMVC/Models/Employee.cs
@@ -0,0 +1,14 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Net.Http.Headers;
+
+namespace MvcMovie.Models;
+
+public class Employee : Person
+{
+    public string? EmployeeID { get; set;}
+    public int Age{get; set;}
+
+
+}
