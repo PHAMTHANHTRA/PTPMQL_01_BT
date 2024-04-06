462fbd2d3d275b681e63678c0e522ddc251a6b3b
“PHAM <2021050646@student.humg.edu.vn>
2024-04-07 00:31:44 +0700

    MVC

--- MVC/MvcMovie/Models/EmployeeModel.cs
+++ MVC/MvcMovie/Models/EmployeeModel.cs
@@ -1,12 +1,17 @@
 using System.ComponentModel.DataAnnotations;
-using DemoMVC.Models;
+using MvcMovie.Models;
 using Microsoft.JSInterop.Infrastructure;
+using MvcMovie.Controllers;
 //Phạm Thanh Trà _ 2021050646
 namespace MvcMovie.Models; 
-    public class EmployeeModel : Person
+    public class EmployeeModel : PersonModel
     {
         
        public string EmployeeID  { get; set; }
        public int Age  { get; set; }
        
     }
+
+public class PersonModel
+{
+}
